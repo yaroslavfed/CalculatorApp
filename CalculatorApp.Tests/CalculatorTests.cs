a50d6fa65f539953517ce2ed6c0001a7d39aa149
Yaroslav Fedoseev <yaroslav.site@mail.ru>
2024-12-22 17:06:24 +0700

    Initial commit

---
+++ CalculatorApp.Tests/CalculatorTests.cs
@@ -0,0 +1,47 @@
+﻿namespace CalculatorApp.Tests;
+
+[TestFixture]
+public class CalculatorTests
+{
+    private Calculator _calculator;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _calculator = new();
+    }
+
+    [Test]
+    public void Add_ShouldReturnCorrectSum()
+    {
+        double result = _calculator.Add(2, 3);
+        Assert.That(result, Is.EqualTo(5));
+    }
+
+    [Test]
+    public void Subtract_ShouldReturnCorrectDifference()
+    {
+        double result = _calculator.Subtract(5, 3);
+        Assert.That(result, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void Multiply_ShouldReturnCorrectProduct()
+    {
+        double result = _calculator.Multiply(4, 3);
+        Assert.That(result, Is.EqualTo(12));
+    }
+
+    [Test]
+    public void Divide_ShouldReturnCorrectQuotient()
+    {
+        double result = _calculator.Divide(10, 2);
+        Assert.That(result, Is.EqualTo(5));
+    }
+
+    [Test]
+    public void Divide_ShouldThrowDivideByZeroException_WhenDividingByZero()
+    {
+        Assert.Throws<DivideByZeroException>(() => _calculator.Divide(10, 0));
+    }
+}
---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CalculatorApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
